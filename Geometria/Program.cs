f33cd807a30d8ab8763f8b83b5202a0488f58299
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-17 15:15:39 +0200

    Esercizio base completato

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Geometria;
+
+
+//*********************** INIZIO PROGRAMMA *******************************************
+int baseRettangolo;
+int altezzaRettangolo;
+string inputUtente;
+
+
+Console.WriteLine("Ciao! Benvenuto nel programma \"calcola il tuo rettangolo!\" Ti prego di inserire i seguenti valori! ");
+do
+{
+    Console.Write("Inserisci la base del rettangolo (maggiore di 0): ");
+    inputUtente = Console.ReadLine();
+    baseRettangolo = int.Parse(inputUtente);
+    Console.Write("Inserisci l'altezza del rettangolo (maggiore di 0): ");
+    inputUtente = Console.ReadLine();
+    altezzaRettangolo = int.Parse(inputUtente);
+} while ((baseRettangolo <= 0) || (altezzaRettangolo <= 0));
+
+
+
+
+Rettangolo myRettangolo = new Rettangolo(baseRettangolo, altezzaRettangolo);
+
+
+
+myRettangolo.CalcolaArea();
+myRettangolo.CalcolaPerimetro();
+
+MostraRettangolo();
+
+
+void MostraRettangolo()
+{
+    Console.WriteLine("------Rettangolo------");
+    Console.WriteLine("base: " + myRettangolo.baseRettangolo + "cm");
+    Console.WriteLine("altezza: " + myRettangolo.altezzaRettangolo + "cm");
+    Console.WriteLine("Perimetro: " + myRettangolo.perimetro + "cm");
+    Console.WriteLine("Area: " + myRettangolo.area + "cm2");
+}
--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace Geometria
 {
-    internal class Rettangolo
+    public class Rettangolo
     {
+        //dichiarazione attributi
+
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        public int area;
+        public int perimetro;
+        //costruttore
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {                
+                this.baseRettangolo = baseRettangolo;
+                this.altezzaRettangolo = altezzaRettangolo;            
+        }
+
+        
+        
+        
+        
+        //metodi
+
+        public void CalcolaArea() //avrei potuto passare i parametri nella funzione mettendo tra parentesi: (int baseRettangolo, int altezzaRettangolo)
+        {
+            
+            area = baseRettangolo * altezzaRettangolo;
+            
+        }
+
+        public void CalcolaPerimetro()
+        {
+            
+            perimetro = (baseRettangolo * 2) + (altezzaRettangolo * 2);
+            
+        }
 
 
 
